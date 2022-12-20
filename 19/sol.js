d5dfc2be20a4d865cf9101c52e8a4d4a9c7860ad
James Baxter <james.baxter@focusrite.com>
2022-12-20 18:14:03 +0800

    Day 19

---
+++ 19/sol.js
@@ -0,0 +1,90 @@
+const originalVals = require("fs")
+  .readFileSync("input.txt", "utf-8")
+  .split("\n")
+  .map((s) => parseInt(s));
+
+const indexMap = originalVals.reduce((accumulator, _, currentIndex) => {
+    accumulator[String(currentIndex)] = currentIndex;
+    return accumulator;
+  },
+  {}
+);
+
+const arr = JSON.parse(JSON.stringify(originalVals));
+for (const [index, val] of originalVals.entries()) {
+  const currentIndex = indexMap[index];
+  let newIndexUnwrapped = (currentIndex + val);
+  if (newIndexUnwrapped < 0) {
+    while (newIndexUnwrapped < 0) {
+      newIndexUnwrapped += originalVals.length;
+    }
+    newIndexUnwrapped -= 1;
+  }
+
+  if (newIndexUnwrapped >= originalVals.length) {
+    while (newIndexUnwrapped >= originalVals.length) {
+      newIndexUnwrapped -= originalVals.length;
+    }
+    newIndexUnwrapped += 1;
+  }
+
+  if (newIndexUnwrapped == 0 && val != 0) {
+    newIndexUnwrapped = originalVals.length - 1;
+  } else if (newIndexUnwrapped == originalVals.length - 1 && val != 0) {
+    newIndexUnwrapped = 0;
+  }
+
+  const newIndex = newIndexUnwrapped % originalVals.length;
+  // console.log(`newIndexUnwrapped: ${newIndexUnwrapped}`);
+  // console.log(`index: ${index}`);
+  // console.log(`val: ${val}`);
+  // console.log(`currentIndex: ${currentIndex}`);
+  // console.log(`newIndex: ${newIndex}`);
+  arr.splice(currentIndex, 1);
+  arr.splice(newIndex, 0, val);
+  // indexMap[index] = currentIndex;
+  // console.log(arr);
+  
+  if (currentIndex < newIndex) {
+    // console.log("move forwards");
+
+    // console.log(Object.entries(indexMap))
+    for (const [k, v] of Object.entries(indexMap)) {
+      if (v > currentIndex && v <= newIndex) {
+        // console.log(k);
+        // console.log(v);
+        // console.log(v - 1);
+        indexMap[k] = v - 1;
+      }
+    }
+
+    indexMap[index] = newIndex;
+
+    // console.log(indexMap);
+
+  } else if (currentIndex > newIndex) {
+    // console.log("move backwards");
+
+    for (const [k, v] of Object.entries(indexMap)) {
+      if (v >= newIndex && v < currentIndex) {
+        // console.log(k);
+        // console.log(v);
+        // console.log(v + 1);
+        indexMap[k] = v + 1;
+      }
+    }
+
+    indexMap[index] = newIndex;
+
+    // console.log(indexMap);
+  }
+}
+console.log(arr)
+const originalIndexOfZero = originalVals.findIndex((v) => v == 0);
+const currentIndexOfZero = indexMap[originalIndexOfZero];
+
+const part1 = [1000, 2000, 3000].reduce((accumulator, currentValue) => {
+  return accumulator += arr[(currentValue + currentIndexOfZero) % arr.length];
+}, 0);
+
+console.log(part1);
